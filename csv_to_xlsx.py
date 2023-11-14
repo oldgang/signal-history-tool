ae9ebc91c8c91706e03729f02fec3109f2eb752f
oldgang <iceman16999@gmail.com>
2023-11-14 14:53:43 +0100

    Fixed a typo in the csv_to_xlsx.py file

--- csv_to_xlsx.py
+++ csv_to_xlsx.py
@@ -3,5 +3,5 @@ import pandas as pd
 def convert_csv_to_xlsx(filename):
     read_file = pd.read_csv(r'filename')
     filename = filename.split('.')[0]
-    filename += .xlsx
+    filename += ".xlsx"
     read_file.to_excel(r'filename', index = None, header=True)
